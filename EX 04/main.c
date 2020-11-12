570f6b22c97090e6f17b4dfe6adb1627f39f2eff
Oscar Araujo <a20844@alunos.ipca.pt>
2020-11-12 21:28:37 +0000

    Correcção EX 04

--- EX 04/main.c
+++ EX 04/main.c
@@ -10,12 +10,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #pragma warning (disable: 4996)
+float areaCir(const a, float b, int c);
+float perimetroCir(const a, float b, int c);
 
-const double Pi 3.14;
-double raio, area, perimetro;
+const float Pi = 3.14;
+float raio;
 
 int main()
-{
+{	
+	float area = areaCir(Pi, raio);
+	float perimetro = perimetroCir(Pi, raio);
 	printf("Introduza o valor real do raio da circunferencia: \n");
 	scanf("%f", &raio);
 	printf("Com raio de %.2f, a area da Circunferencia e %.2f e o Perimetro e %.2f", raio, area, perimetro);
@@ -23,10 +27,14 @@ int main()
 
 }
 
-#pragma section (Funcoes)
+#pragma region (Funcoes)
+float areaCir(const float x, float y, int c)
+{
+	return (pow((x*y),2));
+}
+float perimetroCir(const float x, float y, int c)
+{
+	return((x * y)*2);
+}
 
-
-
-
-
-#pragma end section
+#pragma endregion
